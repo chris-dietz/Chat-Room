b16e0300ce4d190c8a8f55eb8b85069682f04fed
Christopher Dietz <25520174+vengefulsystems@users.noreply.github.com>
2020-12-05 14:03:00 -0700

    Made messages arraylist in MessageStorageBackend private and final.

--- src/main/java/MessageStorageBackend.java
+++ src/main/java/MessageStorageBackend.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 
 public class MessageStorageBackend {
-    List<Message> messages;
+    private final List<Message> messages;
 
     MessageStorageBackend(){
         messages = new ArrayList<>(128);
