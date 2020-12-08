4ab3345e6f10b438d357cdba1226b3907a8652ef
Tyler Craig <Tyler.Craig@colostate.edu>
2020-12-08 10:17:57 -0700

    Fix indentation

--- src/main/resources/webapp/script.js
+++ src/main/resources/webapp/script.js
@@ -1,46 +1,46 @@
 function eleven() {
-    document.getElementById("demo").innerHTML += "<br/><br/>Goodbye World!";
+  document.getElementById("demo").innerHTML += "<br/><br/>Goodbye World!";
 }
 function two() {
-    document.getElementById("demo").innerHTML += "<br/><br/>YAY!";
+  document.getElementById("demo").innerHTML += "<br/><br/>YAY!";
 }
 
 function createProfile() {
-    data = $("#registrationForm").serializeArray();
-    // manually converting the data into a form that is readable by the backend
-    var startingPoint = JSON.stringify(data).indexOf("from") + 11 + "from".length;
-    var endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
-    var from = JSON.stringify(data).substring(startingPoint, endingPoint);
+  data = $("#registrationForm").serializeArray();
+  // manually converting the data into a form that is readable by the backend
+  var startingPoint = JSON.stringify(data).indexOf("from") + 11 + "from".length;
+  var endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
+  var from = JSON.stringify(data).substring(startingPoint, endingPoint);
 
-    startingPoint = JSON.stringify(data).indexOf("subject") + 11 + "subject".length;
-    endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
-    var subject = JSON.stringify(data).substring(startingPoint, endingPoint);
+  startingPoint = JSON.stringify(data).indexOf("subject") + 11 + "subject".length;
+  endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
+  var subject = JSON.stringify(data).substring(startingPoint, endingPoint);
 
-    startingPoint = JSON.stringify(data).indexOf("body") + 11 + "body".length;
-    endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
-    var body = JSON.stringify(data).substring(startingPoint, endingPoint);
+  startingPoint = JSON.stringify(data).indexOf("body") + 11 + "body".length;
+  endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
+  var body = JSON.stringify(data).substring(startingPoint, endingPoint);
 
-    startingPoint = JSON.stringify(data).indexOf("thread") + 11 + "thread".length;
-    endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
-    var thread = JSON.stringify(data).substring(startingPoint, endingPoint);
+  startingPoint = JSON.stringify(data).indexOf("thread") + 11 + "thread".length;
+  endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
+  var thread = JSON.stringify(data).substring(startingPoint, endingPoint);
 
-    startingPoint = JSON.stringify(data).indexOf("room") + 11 + "room".length;
-    endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
-    var room = JSON.stringify(data).substring(startingPoint, endingPoint);
-    data = "{ \"type\":\"group_message\", \"from\":\"" + from + "\", \"subject\":\"" + subject + "\", \"body\":\"" + body + "\", \"thread\":\"" + thread + "\", \"room\":\"" + room + "\"}";
+  startingPoint = JSON.stringify(data).indexOf("room") + 11 + "room".length;
+  endingPoint = nextIndexOf(JSON.stringify(data), startingPoint, "\"");
+  var room = JSON.stringify(data).substring(startingPoint, endingPoint);
+  data = "{ \"type\":\"group_message\", \"from\":\"" + from + "\", \"subject\":\"" + subject + "\", \"body\":\"" + body + "\", \"thread\":\"" + thread + "\", \"room\":\"" + room + "\"}";
 
-    $.ajax({
-        url:         'http://localhost:8080/send_chat',
-        method:      'POST',
-        dataType:    'group_message',
-        data:         data,
-        success:      function() { two(); },
-        error:        function(error) { /*window.location.replace("invalid.html");*/ }
-    });
-    window.alert("Your message has been sent!");
+  $.ajax({
+    url:         'http://localhost:8080/send_chat',
+    method:      'POST',
+    dataType:    'group_message',
+    data:         data,
+    success:      function() { two(); },
+    error:        function(error) { /*window.location.replace("invalid.html");*/ }
+  });
+  window.alert("Your message has been sent!");
 }
 
 function nextIndexOf(strToIndex, index, char) {
-    var newString = strToIndex.substring(index, strToIndex.length);
-    return newString.indexOf(char) + index;
+  var newString = strToIndex.substring(index, strToIndex.length);
+  return newString.indexOf(char) + index;
 }
